9454a6c9a2b02eff153ead7cc0a8587bea247094
Aranya <aranyamal32@gmail.com>
2026-06-07 23:27:18 +0530

    Fix model import case sensitivity

--- Backened/controllers/interviewController.js
+++ Backened/controllers/interviewController.js
@@ -1,5 +1,5 @@
 // controllers/interviewController.js
-const Interview = require("../models/Interview");
+const Interview = require("../models/interview");
 
 // ================= SAVE INTERVIEW =================
 exports.saveInterview = async (req, res) => {
